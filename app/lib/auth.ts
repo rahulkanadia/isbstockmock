ccfb1c8494287e1e06d32e8632d971f7acd57772
Rahul Kanadia <huckster@outlook.com>
2026-01-19 09:02:30 +0530

    Create auth.ts

---
+++ app/lib/auth.ts
@@ -0,0 +1,11 @@
+import { AuthOptions } from "next-auth";
+import DiscordProvider from "next-auth/providers/discord";
+
+export const authOptions: AuthOptions = {
+  providers: [
+    DiscordProvider({
+      clientId: process.env.DISCORD_CLIENT_ID!,
+      clientSecret: process.env.DISCORD_CLIENT_SECRET!,
+    }),
+  ],
+};
