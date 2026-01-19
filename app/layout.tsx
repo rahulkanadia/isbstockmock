0fca176aaaaa1532d4eed1e61f9e35679be3c4c0
Rahul Kanadia <huckster@outlook.com>
2026-01-19 09:03:14 +0530

    Create layout.tsx

---
+++ app/layout.tsx
@@ -0,0 +1,16 @@
+import "./globals.css";
+import { ReactNode } from "react";
+import { Header } from "./components/Header";
+
+export default function RootLayout({ children }: { children: ReactNode }) {
+  return (
+    <html lang="en">
+      <body className="bg-zinc-950 text-zinc-100">
+        <Header />
+        <main className="max-w-7xl mx-auto px-6 py-6">
+          {children}
+        </main>
+      </body>
+    </html>
+  );
+}
